d228e8a88ff2c0b241eb467a2b566fa45d8bce7e
vikramsamak <vikramsamak02@gmail.com>
2024-02-27 11:13:14 +0530

    Code Upadted.

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const validUrl = require("valid-url");
+const shortid = require("shortid");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+var urlDatabase = {};
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// Endpoint to save shorturl
+app.post("/api/shorturl", function (req, res) {
+  const { url } = req.body;
+
+  if (!validUrl.isUri(url)) {
+    return res.status(400).json({ error: "invalid url" });
+  }
+
+  const shortUrl = shortid.generate();
+
+  urlDatabase[shortUrl] = url;
+
+  res.json({ original_url: url, short_url: shortUrl });
+});
+
+// Endpoint to redirect to original url
+app.get("/api/shorturl/:shorturl", function (req, res) {
+  const { shorturl } = req.params;
+  const original_url = urlDatabase[shorturl];
+
+  if (original_url) {
+    res.redirect(original_url);
+  } else {
+    res.status(404).json({ error: "invalid url" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
