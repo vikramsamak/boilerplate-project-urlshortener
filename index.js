8cc3e88d3d2be6edd071e9df895fc4e16a559cf3
vikramsamak <vikramsamak02@gmail.com>
2024-02-27 11:25:59 +0530

    Code Updated.

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api", function (req, res) {
+  res.json({ error: "invalid url" });
 });
 
 // Endpoint to save shorturl
